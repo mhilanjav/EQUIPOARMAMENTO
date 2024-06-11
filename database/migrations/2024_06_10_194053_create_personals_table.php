baa008783ad7e44a47d47055ad9c05518a05d85f
mhilanjav <milanjav2511@gmail.com>
2024-06-10 21:31:49 -0400

    “avance”

---
+++ database/migrations/2024_06_10_194053_create_personals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personals', function (Blueprint $table) {
+            $table->id();
+            $table->string("ecalafon",15);
+            $table->string("nombres",45);
+            $table->string("apellidos",45);
+            $table->timestamps();
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personals');
+    }
+};
