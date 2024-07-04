295c611493b993d59713be107c316c20837895f6
mhilanjav <milanjav2511@gmail.com>
2024-07-04 13:15:37 -0400

    “avance”

--- resources/views/personal/crear.blade.php
+++ resources/views/personal/crear.blade.php
@@ -1,6 +1,13 @@
 @extends('adminlte::page')
 @section("css")
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.min.css" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+
+
     @endsection
 
 @section('title', 'Dashboard')
@@ -28,6 +35,18 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/js/plugins/buffer.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/js/plugins/filetype.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/js/plugins/piexif.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/js/plugins/sortable.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/js/fileinput.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.5.0/js/locales/LANG.js"></script>
+    <script>
+        $.fn.fileinputBsVersion = "3.3.7"; // if not set, this will be auto-derived
+        $("#input-id").fileinput();
+        $("#input-id").fileinput({'showUpload':false, 'previewFileType':'any'});
+    <script>
 @stop
 
--- resources/views/personal/form.blade.php
+++ resources/views/personal/form.blade.php
@@ -1,24 +1,57 @@
-<div class="form-floating mb-3">
-    <input type="text" name="escalafon" id="escalafon" class="form-control">
-    <label for="floatingInput">Nombres</label>
-</div>
-<div class="form-floating mb-3">
-    <input type="text" name="escalafon" id="escalafon" class="form-control">
-    <label for="floatingInput">Apellidos</label>
-</div>
-<div class="form-floating mb-3">
-    <input type="text" name="escalafon" id="escalafon" class="form-control">
-    <label for="floatingInput">Escalafon</label>
-</div>
-<div class="form-floating mb-3">
-    <select class="form-select" aria-label="Default select example">
-        <option value="">Elija  un grado</option>
-        @foreach ($grados as $grado)
-            <option value="">{{  $grado->grado }}</option>
-        @endforeach
-    </select>
-    <label for="floatingInput">Grados</label>
-</div>
 
+
+
+<div class="row">
+    <div class="col-5">
+        <div class="form-floating mb-3">
+            <select class="form-select" aria-label="Default select example">
+                <option value="">Elija  un grado</option>
+                @foreach ($grados as $grado)
+                    <option value="">{{  $grado->grado }}</option>
+                @endforeach
+            </select>
+            <label for="floatingInput">Grados</label>
+        </div>+
+        <div class="form-floating mb-3">
+            <input type="text" name="escalafon" id="escalafon" class="form-control">
+            <label for="floatingInput">Nombres</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="text" name="escalafon" id="escalafon" class="form-control">
+            <label for="floatingInput">Apellidos</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="text" name="escalafon" id="escalafon" class="form-control">
+            <label for="floatingInput">Escalafon</label>
+        </div>
+    </div>
+    <div class="col-4">
+        <input id="input-id" type="file" class="file" data-preview-file-type="text">
+    </div>
+    <div class="col-3">
+        <div class="row">
+            <div class="col-12">
+                <button type="button" class="col-8 mb-2 btn btn-outline-success"><i class="fa-solid fa-trash-can"></i> &nbsp;BUSCAR</button>
+            </div>
+            <div class="col-12">
+                <button type="button" class="col-8 mb-2 btn btn-outline-success"><i class="fa-solid fa-file-import"></i> &nbsp;AGREGAR</button>
+            </div>
+            <div class="col-12">
+                <button type="button" class="col-8  mb-2 btn btn-outline-success"><i class="fa-solid fa-download"></i>&nbsp;IMPORTAR</button>
+            </div>
+            <div class="col-12">
+                <button type="button" class="col-8 mb-2 btn btn-outline-success"><i class="fa-solid fa-file-import"></i> &nbsp;EXPORTAR</button>
+            </div>
+            <div class="col-12">
+                <button type="button" class="btn btn-outline-success  mb-2 col-8"><i class="fa-solid fa-trash-arrow-up"></i> &nbsp;ELIMINAR</button>
+            </div>
+            <div class="col-12">
+                <button type="button" class="btn btn-outline-success mb-2 col-8"> <i class="fa-solid fa-magnifying-glass"></i>&nbsp;FOTO</button>
+            </div>
+            <div class="col-12">
+                <button type="button" class="btn btn-outline-success  mb-2 col-8">ATRAS</button>
+            </div>
+    </div>
+</div>
 
 
